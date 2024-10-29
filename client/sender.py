147939b8b80b0f098eda4da8aebda1dfd3e28ecf
Pelangi Masita <pelangimasita18@gmail.com>
2024-10-29 18:52:23 +0700

    encrypting and decrypting

--- client/sender.py
+++ client/sender.py
@@ -1,21 +1,22 @@
 import socket
-from des.DES import DES  
-from des.utils import pad_string 
+from des.DES import DES
+from des.utils import pad_string
 
 IP = "127.0.0.1"
 PORT = 65432
-DES_KEY = 12345678 
+DES_KEY = 12345678  
 
 def main():
-    des_instance = DES(DES_KEY) 
+    des_instance = DES(DES_KEY)
     message = "This is a longer message that needs to be encrypted."
-
-    padded_message = pad_string(message)
-    encrypted_message = des_instance.encrypt(padded_message)
+    padded_message = pad_string(message)  
     
+    encrypted_chunks = [des_instance.encrypt(padded_message[i:i+64]) for i in range(0, len(padded_message), 64)]
+    encrypted_message = ''.join(encrypted_chunks)  
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((IP, PORT))
-        s.sendall(encrypted_message.encode())
+        s.sendall(encrypted_message.encode())  
         print("Encrypted message sent:", encrypted_message)
 
 if __name__ == "__main__":
--- server/receiver.py
+++ server/receiver.py
@@ -7,7 +7,7 @@ PORT = 65432
 DES_KEY = 12345678 
 
 def main():
-    des_instance = DES(DES_KEY)  
+    des_instance = DES(DES_KEY)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((IP, PORT))
@@ -20,8 +20,9 @@ def main():
             encrypted_message = conn.recv(1024).decode()
             print("Received encrypted message:", encrypted_message)
 
-            decrypted_binary = des_instance.decrypt(encrypted_message)
-            decrypted_message = bin_to_text(decrypted_binary)
+            decrypted_chunks = [des_instance.decrypt(encrypted_message[i:i+64]) for i in range(0, len(encrypted_message), 64)]
+            decrypted_binary = ''.join(decrypted_chunks)
+            decrypted_message = bin_to_text(decrypted_binary)  
             print("Decrypted message:", decrypted_message)
 
 if __name__ == "__main__":
