2d236b12110297f748225e2db77090e26d7c43a8
Pelangi Masita <pelangimasita18@gmail.com>
2024-12-13 12:05:01 +0700

    rsa

---
+++ revisi/rsa/key_generation.py
@@ -0,0 +1,19 @@
+from utils.math_utils import gcd, mod_inverse
+
+def rsa_key_generation():
+    """Generate RSA public and private keys."""
+    p = 61
+    q = 53
+
+    n = p * q                 
+    phi = (p - 1) * (q - 1)   
+
+    e = 17
+    if gcd(e, phi) != 1:
+        raise ValueError("e and phi(n) are not coprime")
+
+    d = mod_inverse(e, phi)
+
+    public_key = (e, n)
+    private_key = (d, n)
+    return public_key, private_key
